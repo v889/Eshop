23d785c9f38e3798a160eef22806334902bebef2
v889 <72309826+v889@users.noreply.github.com>
2022-08-24 15:53:17 +0530

    Initial commit

---
+++ store/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import *
+
+class ProductView(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+        'category',
+        'image',
+    ]
+class CategoryView(admin.ModelAdmin):
+    list_display =[
+        'name',
+    ]
+admin.site.register(Product,ProductView)
+admin.site.register(Category,CategoryView)
+admin.site.register(Customer)
+admin.site.register(Order)
+
+# Register your models here.
---
+++ store/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from store.middlewares.auth import auth_middleware
+urlpatterns= [
+path('', views.Index.as_view(), name='homepage'),
+    path('store/', views.store , name='store'),
+
+    path('signup/', views.Signup.as_view(), name='signup'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.logout , name='logout'),
+    path('cart/', auth_middleware(views.Cart.as_view()) , name='cart'),
+    path('check-out/', views.CheckOut.as_view() , name='checkout'),
+    path('orders/', auth_middleware(views.OrderView.as_view()), name='orders'),
+
+]
